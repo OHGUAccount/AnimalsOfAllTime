0c3221c521fde17f1dcce1fd6b062762b249fc91
Nabeel Rahman <2739548r@student.gla.ac.uk>
2024-02-27 22:40:07 +0000

    feat: add models

--- animals_of_all_time/models.py
+++ animals_of_all_time/models.py
@@ -1,3 +1,86 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.template.defaultfilters import slugify
+    
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+
+class Animal(models.Model):
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    name = models.CharField(max_length= 128, unique=True)
+    description = models.TextField(blank=True)
+    picture = models.ImageField(upload_to='animal_images', blank=True)
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    date = models.DateField(auto_now_add=True)
+    slug = models.SlugField(unique=True)
+
+    @property
+    def total_votes(self):
+        return self.upvotes - self.downvotes
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Animal, self).save(*args, **kwargs)
+
+
+class Discussion(models.Model):
+    title = models.CharField(max_length=128)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    description = models.TextField(blank=True)
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Discussion, self).save(*args, **kwargs)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    upvoted_by = models.ManyToManyField(UserProfile)
+    downvotes_by = models.ManyToManyField(UserProfile)
+
+    def __str__(self):
+        return self.content
+    
+
+class UserList(models.Model):
+    title = models.CharField(max_length=128)
+    date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    animals = models.ManyToManyField(Animal)
+    description = models.TextField(blank=True)
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    upvoted_by = models.ManyToManyField(UserProfile)
+    downvotes_by = models.ManyToManyField(UserProfile)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(UserList, self).save(*args, **kwargs)
--- wad2_project/settings.py
+++ wad2_project/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'animals_of_all_time'
+    'animals_of_all_time',
+    'registration'
 ]
 
 MIDDLEWARE = [
@@ -63,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media', 
             ],
         },
     },
@@ -119,3 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
+LOGIN_URL = 'animals-of-all-time:login'
+
+REGISTRATION_OPEN = True
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = 'animals-of-all-time:index'
+LOGIN_URL = 'auth_login'
