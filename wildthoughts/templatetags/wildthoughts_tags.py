9b5cc65872df3cc709e09ecf48b4dfdc7065a8bb
Nabeel Rahman <2739548r@student.gla.ac.uk>
2024-03-06 00:05:49 +0000

    fix: navbar

--- wildthoughts/templatetags/wildthoughts_tags.py
+++ wildthoughts/templatetags/wildthoughts_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from ..models import UserProfile
 
 register = template.Library()
 
@@ -7,3 +8,11 @@ def theme(context):
     request = context['request']
     theme = request.COOKIES.get('theme', 'light')
     return theme
+
+@register.simple_tag(takes_context=True)
+def profile_picture(context):
+    request = context['request']
+    profile = UserProfile.objects.get(user=request.user)
+    if profile.picture:
+        return profile.picture
+    return None
