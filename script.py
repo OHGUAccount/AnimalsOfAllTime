95c7d9c5a8cce6db491632e661910e0bae5576fb
Nabeel Rahman <2739548r@student.gla.ac.uk>
2024-03-13 00:11:12 +0000

    refactor: better generated profiles in script.py

--- script.py
+++ script.py
@@ -20,48 +20,72 @@ from PIL import Image
 from wildthoughts.models import Animal, Discussion, Petition, UserList, UserProfile
 
 
-class ImageProfile:
-    # class dedicated to create image profiles with random colours using pillow
-    colours = ['blue', 'green', 'red', 'yellow']
+class ProfileDownloader:
+    @classmethod
+    def __get_json(cls, count):
+        url = f'https://randomuser.me/api/?inc=email,login,picture&results={count}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data
 
     @classmethod
-    def create_colour(cls, colour: str, width=300, height=300) -> None:
-        image = Image.new('RGB', (width, height), colour)
-        image.save(f'media\\profile_images\\{colour}.png')
-
+    def __save_image(cls, entry) -> str:
+        img_url = entry['picture']['large']
+        with requests.get(img_url, stream=True) as response:
+            filename = os.path.join('media\\profile_images', os.path.basename(img_url))
+            with open(filename, 'wb') as out_file:
+                out_file.write(response.content)
+            img_path = os.path.join('profile_images', os.path.basename(img_url))
+            return img_path
+        
     @classmethod
-    def create_multi_colours(cls, colours: list[str] = None, width=300, height=300):
-        if not colours:
-            colours = cls.colours
-        for colour in colours:
-            cls.create_colour(colour, width, height)
-
+    def __get_data(cls, entry) -> tuple[str, str, str]:
+        try:
+            username = entry['login']['username']
+            email = entry['email']
+            password = entry['login']['password']
+            if username and password:
+                image_path = cls.__save_image(entry)
+                if image_path:
+                    return username, email, password, image_path
+        except:
+            pass
+        
     @classmethod
-    def exist_colour(cls, colour: str, create=False) -> str:
-        exist = os.path.exists(f'media\\profile_images\\{colour}.png')
-        if not exist and create:
-            cls.create_colour(colour)
-            exist = os.path.exists(f'media\\profile_images\\{colour}.png')
-            
-        if exist:
-            return f'profile_images\\{colour}.png'
-    
-    @classmethod
-    def exist_multi_colours(cls, colours: list[str] = None, create=False) -> dict[str, bool]:
-        if not colours:
-            colours = cls.colours
+    def download(cls, count=50) -> dict[str, dict[str, str]]:
+        profile_dict = cls.__get_json(count)
         output_dict = {}
-        for colour in colours:
-            output_dict[colour] = cls.exist_colour(colour, create)
-        return output_dict
-    
-    @classmethod
-    def random_colour(cls, colours: list[str] = None) -> tuple[str, str]:
-        if not colours:
-            colours = cls.colours
-        colour = random.choice(colours)
-        return colour, cls.exist_colour(colour, create=True)
 
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(cls.__get_data, entry) for entry in profile_dict['results']]
+
+            for future in concurrent.futures.as_completed(futures):
+                result = future.result() 
+                if result is not None:
+                    username, email, password, image_path = result
+                    output_dict[username] = {
+                        'email': email,
+                        'password' : password,
+                        'image_path' : image_path
+                    }
+
+        with open("profile.json", "w") as f:
+            json.dump(output_dict, f, indent=2)
+        return output_dict
+
+    @classmethod
+    def clear(cls) -> None:
+        if os.path.exists("profile.json"):
+            os.remove("profile.json")
+
+        for item in os.listdir("media\\profile_images"):
+            item_path = os.path.join("media\\profile_images", item)
+            if os.path.isfile(item_path):
+                os.remove(item_path)
+            else:
+                shutil.rmtree(item_path)
+        
 
 class AnimalDownloader:
     """
@@ -155,10 +179,23 @@ class AnimalDownloader:
             json.dump(output_dict, f, indent=2)
         return output_dict
 
+    @classmethod
+    def clear(cls) -> None:
+        if os.path.exists("animal.json"):
+            os.remove("animal.json")
+
+        for item in os.listdir("media\\animal_images"):
+            item_path = os.path.join("media\\animal_images", item)
+            if os.path.isfile(item_path):
+                os.remove(item_path)
+            else:
+                shutil.rmtree(item_path)
+
 
 class Database:
     # class dedicated to populate and migrate database
     animal_dict: dict = None
+    profile_dict: dict = None
     adjectives = ['scariest', 'gorgeous', 'fastest', 'slowest']
     animals: list[Animal] = None
     users: list[UserProfile] = None
@@ -182,6 +219,13 @@ class Database:
         cls.migrate()
 
     @classmethod
+    def load_profile_dict(cls) -> dict:
+        if not cls.profile_dict:
+            with open("profile.json", "r") as f:
+                cls.profile_dict = json.load(f)
+        return cls.profile_dict
+
+    @classmethod
     def load_animal_dict(cls) -> dict:
         if not cls.animal_dict:
             with open("animal.json", "r") as f:
@@ -200,15 +244,16 @@ class Database:
     
     @classmethod
     def add_users(cls) -> None:
-        for num in range(100):
+        profile_dict = cls.load_profile_dict()
+        for username, data in profile_dict.items():
             user, created = User.objects.get_or_create(
-                username = f'test{num}',
-                email = f'test{num}@gmail.com',
-                password = 'WAD2Test2024',
+                username = username,
+                email = data['email'],
+                password = data['password'],
             )
+
             user_profile, created = UserProfile.objects.get_or_create(user=user)
-            colour, image_path = ImageProfile.random_colour()
-            user_profile.picture = image_path
+            user_profile.picture = data['image_path']
             user_profile.save()
 
     @classmethod
@@ -278,11 +323,12 @@ class Database:
                 author=user,
                 picture=animal.picture,
                 description="This animal is at the risk of extinction!",
-                goal=200,
-                signatures=random.randint(0, 100),
-                slug=slugify(f'Petition for {animal.name}')
+                slug=slugify(f'Petition for {animal.name} by {user.user.username}')
             )
+            petition.goal = 100
+            petition.signatures = random.randint(0, 100)
             petition.animals.add(animal)
+            petition.save()
 
     @classmethod
     def populate(cls) -> None:
@@ -311,10 +357,13 @@ class Script:
     @classmethod
     def execute(cls, action):
         if action == 'download':
+            ProfileDownloader.download()
             AnimalDownloader.download()
 
         elif action == 'clear':
             Database.clear()
+            ProfileDownloader.clear()
+            AnimalDownloader.clear()
 
         elif action == 'populate':
             Database.populate()
@@ -327,8 +376,15 @@ class Script:
             Database.migrate()
         
         elif action == 'all':
+            Database.clear()
+            ProfileDownloader.clear()
+            AnimalDownloader.clear()
+
             animal_dict = AnimalDownloader.download()
+            profile_dict = ProfileDownloader.download()
+
             Database.animal_dict = animal_dict
+            Database.profile_dict = profile_dict
             Database.populate()
             Database.migrate()
 
