0daac44db65c86fbd806ee08f415078d2bb43e80
Nabeel Rahman <2739548r@student.gla.ac.uk>
2024-03-13 12:12:35 +0000

    refactor: forms

--- wildthoughts/forms.py
+++ wildthoughts/forms.py
@@ -75,8 +75,7 @@ class PetitionForm(forms.ModelForm):
             'decision_maker': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
             'goal': forms.NumberInput(attrs={'class': 'form-control', 'min': 10}),
-            'animals': select2forms.SelectMultiple(attrs={'data-placeholder': 'Please select Animals',
-                                                          'style': '"width: 100%"'})
+            'animals': forms.SelectMultiple(attrs={'class': 'form-control'})
         }
 
 
@@ -88,6 +87,5 @@ class UserListForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
-            'animals': select2forms.SelectMultiple(attrs={'data-placeholder': 'Please select Animals',
-                                                          'style': '"width: 100%"'})
+            'animals': forms.SelectMultiple(attrs={'class': 'form-control'})
         }
