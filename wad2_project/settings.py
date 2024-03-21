325c34f540b1deec561ea79bb225aef64cafc633
Nabeel Rahman <157132682+2739548r@users.noreply.github.com>
2024-03-21 18:06:47 +0000

    fix: secret key path

--- wad2_project/settings.py
+++ wad2_project/settings.py
@@ -23,8 +23,9 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+KEY_PATH = os.path.join(BASE_DIR, 'wad2_project\\secret.key')
 key = None
-with open('secret.key') as f:
+with open(KEY_PATH) as f:
     key = f.read().strip()
 SECRET_KEY = key
 
